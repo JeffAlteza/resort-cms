5b9ad6c6d9088f1603b5557456a9502bcb4139d8
JeffAlteza <jepoyalteza@gmail.com>
2023-11-30 20:02:37 +0800

    ✨

--- resources/views/components/book-now.blade.php
+++ resources/views/components/book-now.blade.php
@@ -1,12 +1,13 @@
-<div class="min-h-[200px] py-10 bg-cover bg-fixed bg-center bg-no-repeat lg:px-36"
-    style="background-image: url('{{ asset('image/martina29.jpg') }}');">
-    <div class="flex flex-col items-center justify-center h-full">
-        <h1 class="text-white text-4xl font-bold mb-5" data-aos="fade-up" data-aos-duration="800"
+<div class="min-h-[200px] py-10  lg:px-36 relative">
+    <div class="bg-cover bg-fixed bg-center bg-no-repeat brightness-75 absolute inset-0"
+        style="background-image: url('{{ asset('image/martina29.jpg') }}');"></div>
+    <div class="flex flex-col items-center justify-center h-full relative z-10">
+        <h1 class="text-white text-4xl font-bold mb-5" data-aos="fade" data-aos-duration="800"
             style="text-shadow: 1px 1px 2px rgb(100, 100, 100);">
             Make your stay unforgettable with us!
         </h1>
-        <a href="#next"
-            class="inline-flex justify-center items-center mt-5 py-2 px-5 text-lg text-white rounded-lg bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-gray-400">
+        <a href="#next" data-aos="fade" data-aos-duration="900"
+            class="inline-flex justify-center items-center py-3 px-5 text-base font-lg font-bold text-center text-white rounded-lg border border-white transition-all hover:border-green-600 hover:bg-green-600 focus:ring-4 focus:ring-gray-400">
             Book Now
         </a>
     </div>
